9c1640951800401509140663f7524542bd02c2c3
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-10-25 12:49:42 +0500

    Empty exception added

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -3,16 +3,17 @@ class Cd < ApplicationRecord
     has_and_belongs_to_many :languages
     has_and_belongs_to_many :speakers
     has_and_belongs_to_many :categories
+    has_many :feedbacks
 
-    has_many :tracks
+    has_many_attached :tracks
 
     has_one_attached :cover
 
-    accepts_nested_attributes_for :tracks, allow_destroy: true
+    # accepts_nested_attributes_for :tracks, allow_destroy: true
 
     def as_json(options = {})
         h = super(options.merge({ except: [:created_at, :updated_at]}))
-        h[:cover] = Rails.application.routes.url_for(controller: 'active_storage/blobs', action: :show, signed_id: cover.signed_id, filename: cover.filename)
+        h[:cover] = cover.attached? ? Rails.application.routes.url_for(controller: 'active_storage/blobs', action: :show, signed_id: cover.signed_id, filename: cover.filename) : ""
         h[:categories] = categories
         h[:speakers] = speakers
         h[:languages] = languages
